df0171ce8254c8bc7b03b20be3b3040e3ff9399a
Khaled_mahran_19 <136863971+kkhaled119@users.noreply.github.com>
2024-09-30 12:17:59 +0300

    install react +tailwind first template

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,11 @@
+// tailwind.config.js
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // جميع الملفات داخل مجلد src
+    "./public/index.html", // في حالة وجود ملف HTML في المجلد public
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};

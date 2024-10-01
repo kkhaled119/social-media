b3ff2448647e177d849abb8b06679d0e607e9a3f
Khaled_mahran_19 <136863971+kkhaled119@users.noreply.github.com>
2024-10-02 02:37:52 +0300

    create user model +controller +test api

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,22 @@
 import express from "express";
 import mongoose from "mongoose";
+import userRoutes from "./routes/userRoute.js"; // تأكد من صحة مسار ملف userRoute.js
 
-mongoose.connect("mongodb://localhost:27017/socialmedia").then(() => {
-  console.log("Mongo is connected ");
-});
+mongoose
+  .connect("mongodb://localhost:27017/socialmedia")
+  .then(() => {
+    console.log("MongoDB is connected");
+  })
+  .catch((err) => {
+    console.error("MongoDB connection error:", err);
+  });
 
 const app = express();
 
 const Port = 5003;
 
+app.use("/user", userRoutes);
+
 app.listen(Port, () => {
-  console.log(`server is running on port  ${Port}`);
+  console.log(`Server is running on port ${Port}`);
 });

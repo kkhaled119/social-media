fc587a9b94ef067b733d7621b9e4af2e466ff321
Khaled_mahran_19 <136863971+kkhaled119@users.noreply.github.com>
2024-10-02 00:29:13 +0300

    connect to data base

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,9 @@
 import express from "express";
+import mongoose from "mongoose";
+
+mongoose.connect("mongodb://localhost:27017/socialmedia").then(() => {
+  console.log("Mongo is connected ");
+});
 
 const app = express();
 
